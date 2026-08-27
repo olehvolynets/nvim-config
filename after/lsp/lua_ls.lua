661e73b5020c7542b58ec2e4351880b14afd1927
Oleh Volynets <saggot91@gmail.com>
2026-08-27 23:26:42 +0200

    control LuaLS inlay hints

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,3 +1,6 @@
+local should_add_runtime_path = vim.uv.cwd() ~= (vim.uv.os_homedir() .. "/devel/neovim")
+local with_hint = vim.env.ENABLE_HINT == "1"
+
 ---@type vim.lsp.Config
 return {
     settings = {
@@ -12,9 +15,7 @@ return {
             -- Make the server aware of Neovim runtime files
             workspace = {
                 checkThirdParty = "ApplyInMemory",
-                library = {
-                  vim.env.VIMRUNTIME
-                }
+                library = should_add_runtime_path and ({ vim.env.VIMRUNTIME }) or ({})
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
@@ -24,7 +25,11 @@ return {
                 displayContext = 20,
                 showWord = "Enable",
             },
-            hint = { enable = false },
+            hint = {
+                enable = with_hint,
+                awaitPropagate = with_hint,
+                setType = with_hint,
+            },
             hover = {
                 expandAlias = false,
             },
